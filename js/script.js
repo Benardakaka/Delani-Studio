cbebcc5006e26a36395761347c948632dd059876
Benardakaka <benardakaka484@gmail.com>
2020-10-09 12:46:12 +0300

    add

--- js/script.js
+++ js/script.js
@@ -57,5 +57,5 @@ $(document).ready(function () {
 
 function main() {
     alert("Hello ,thank you for contacting us ,please click 'OK' to Sign in");
-    alert("Mr.Benard Akaka will contact you shortly");
+    // alert("Mr.Benard Akaka will contact you shortly");
 }
