17221c16f259b89fff1b7b113ee0a533c5bc0451
Benardakaka <benardakaka484@gmail.com>
2020-10-12 15:07:30 +0300

    add

--- js/script.js
+++ js/script.js
@@ -76,7 +76,7 @@
 });
 
 function main() {
-    alert("Hello, we have received your message. Thank you for reaching out to us. **");
+    alert("Hello, we have received your message. Thank you for reaching out to us. *");
 }
 
 
