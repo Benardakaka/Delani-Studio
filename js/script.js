d12027116a46f917639369de199caa7fbb38282a
Benardakaka <benardakaka484@gmail.com>
2020-10-13 22:59:42 +0300

    add

--- js/script.js
+++ js/script.js
@@ -79,7 +79,6 @@ function main() {
     var name = document.getElementById("name").value;
     if(name){
     alert("Hello,"+ name + " we have received your message. Thank you for reaching out to us. *");
-
 } 
 }
 
